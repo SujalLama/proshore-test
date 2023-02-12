328926a2aed61c1204637bdaf9718a4bd631f66c
Sujal Lama <lsujal41@gmail.com>
2023-02-12 19:02:03 +0545

    axios timeout increased

--- src/lib/Axios.js
+++ src/lib/Axios.js
@@ -4,7 +4,7 @@ const GitHubURL = process.env.REACT_APP_GITHUB_API_URL;
 
 const AxiosGitHub = axios.create({
   baseURL: GitHubURL,
-  timeout: 1000,
+  timeout: 2000,
   Accept: "application/vnd.github+json",
 });
 
